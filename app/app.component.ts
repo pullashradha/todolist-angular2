0b0cad82242b0150ae0ce7910b1e75c03ad93a36
Shradha Pulla <pullashradha@gmail.com>
2016-08-10 14:03:31 -0700

    Add to app.component.ts file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,28 @@ import {Component} from "angular2/core";
 
 @Component ({
   selector: "my-app",
-  template: `<h1>Skeleton Angular2 App!</h1>`
+  template:
+    `<div class="container">
+      <h1>To-Do List</h1>
+      <h3> *ngFor="#task of tasks"{{task.description}}</h3>
+    </div>`
 })
 
-export class AppComponent {
+export class Task {
+  public done: boolean = false;
+  constructor (public description: string, public id: number) {
 
+  }
+}
+
+export class AppComponent {
+  public tasks: Task[];
+  constructor() {
+    this.tasks = [
+      new Task("Create To-Do List app", 0),
+      new Task("Learn Kung Fu", 1),
+      new Task("Rewatch all the Lord of the Rings movies", 2),
+      new Task("Do the laundry", 3)
+    ];
+  }
 }
